76ea31f9f64ef04d8a1b3cd4bef8e061647fa983
Malcolm Fischer <mtfische@ualberta.ca>
2019-02-06 18:27:13 -0700

    Lab3 complete

--- src/main/java/cmput402/tdd/Utility.java
+++ src/main/java/cmput402/tdd/Utility.java
@@ -1,19 +1,39 @@
 package cmput402.tdd;
 
 import java.util.HashMap;
-
+import java.util.ArrayList;
 import cmput402.tdd.service.Search;
 
 public class Utility {
 
-	public HashMap<String, Integer>  countAverageScore(Search search){
-		
-		return null;
+	public HashMap<String, Float>  countAverageScore(Search search){
+		HashMap<String, ArrayList<Integer>> scores= search.returnAll();
+		HashMap<String, Float> result = new HashMap<String, Float>();
+		Float avg;
+
+		for(String player : scores.keySet()){
+			avg = (float)scores.get(player).get(1) / scores.get(player).get(0);
+			System.out.println(avg);
+			result.put(player, avg);
+		}
+
+		return result;
 	}
 	
-public HashMap<String, Integer>  findTopScorer(Search search){
-		
-		return null;
+	public HashMap<String, Integer>  findTopScorer(Search search){
+		HashMap<String, ArrayList<Integer>> scores = search.returnAll();
+		HashMap<String, Integer> result = new HashMap<String, Integer>();
+		Integer score = 0;
+		String topPlayer = "";
+
+		for(String player : scores.keySet()){
+			if (scores.get(player).get(1) > score){
+				score = scores.get(player).get(1);
+				topPlayer = player;
+			}
+		}
+		result.put(topPlayer, score);
+		return result;
 	}
 	
 }
--- src/test/java/cmput402/tdd/AppTest.java
+++ src/test/java/cmput402/tdd/AppTest.java
@@ -1,9 +1,16 @@
 package cmput402.tdd;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import java.util.ArrayList;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
 
+import cmput402.tdd.service.Search;
 /**
  * Unit test for simple App.
  */
@@ -34,5 +41,63 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+
+    public void testCountAverageScore()
+    {
+        Utility util = new Utility();
+        Search mockSearch = mock(Search.class);
+        HashMap<String, ArrayList<Integer>> fakemap = new HashMap<String,ArrayList<Integer>>();
+        ArrayList<Integer> stat1 = new ArrayList<Integer>();
+        stat1.add(50);
+        stat1.add(60);
+
+        ArrayList<Integer> stat2 = new ArrayList<Integer>();
+        stat2.add(10);
+        stat2.add(80);
+
+        ArrayList<Integer> stat3 = new ArrayList<Integer>();
+        stat3.add(20);
+        stat3.add(10);
+
+        fakemap.put("messi1", stat1);
+        fakemap.put("messi2", stat2);
+        fakemap.put("messi3", stat3);
+        
+        when(mockSearch.returnAll()).thenReturn(fakemap);
+        HashMap<String, Float> averages = util.countAverageScore(mockSearch);
+
+        assertTrue(averages.get("messi1") == 1.2f);
+        assertTrue(averages.get("messi2") == 8.0f);
+        assertTrue(averages.get("messi3") == 0.5f);
+    }
+
+    public void testFindTopScore()
+    {
+        Utility util = new Utility();
+        Search mockSearch = mock(Search.class);
+        HashMap<String, ArrayList<Integer>> fakemap = new HashMap<String,ArrayList<Integer>>();
+        ArrayList<Integer> stat1 = new ArrayList<Integer>();
+        stat1.add(50);
+        stat1.add(60);
+
+        ArrayList<Integer> stat2 = new ArrayList<Integer>();
+        stat2.add(10);
+        stat2.add(80);
+
+        ArrayList<Integer> stat3 = new ArrayList<Integer>();
+        stat3.add(20);
+        stat3.add(10);
+
+        fakemap.put("messi1", stat1);
+        fakemap.put("messi2", stat2);
+        fakemap.put("messi3", stat3);
+        
+        when(mockSearch.returnAll()).thenReturn(fakemap);
+        HashMap<String, Integer> averages = util.findTopScorer(mockSearch);
+        String player = averages.keySet().iterator().next(); 
+
+        assertTrue(player == "messi2");
+        assertTrue(averages.get(player) == 80);
     }
 }
